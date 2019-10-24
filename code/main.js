0fff65ab0d51c07921c117587a2f8d607ac5fcb3
Mesfer <mesfer.vip@gmail.com>
2019-10-24 16:35:20 +0300

    axios-api-done

--- code/main.js
+++ code/main.js
@@ -1,2 +1,36 @@
 // If you get stuck go to axiox documentations
 // https://github.com/axios/axios
+
+const baseUrl = `https://sei-relativity-ruh.herokuapp.com/developers`
+// const axios = require('axios').default;
+
+
+// Make a request for a user with a given ID
+axios.get('https://sei-relativity-ruh.herokuapp.com/developers')
+  .then(function (response) {
+    // handle success
+    
+    console.log(response.data.forEach(element => {
+        console.log(element.name);
+    }));
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+ 
+
+const testPost = function(){
+  axios.post('https://sei-relativity-ruh.herokuapp.com/developers', {
+    name: "Mesfer",
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+testPost();
+
